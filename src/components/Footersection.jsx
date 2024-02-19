ae0a14dc0905c8419012f91ab1a2788b32335b16
sanjuverma373 <sv0240866@gmail.com>
2024-02-19 10:02:51 +0530

    footer modified

--- src/components/Footersection.jsx
+++ src/components/Footersection.jsx
@@ -18,19 +18,19 @@ const Footersection = () => {
                                         <div className=' pt-6 lg:pt-0 flex justify-between'>
                                                 <ul>
                                                         <li className=' pb-[10px] sm:pb-[20px] text-white font-Recharge font-bold text-[16px] sm:text-[18px] leading-[21px]' href="">Quick links</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300' >Home</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300' >Features</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300' >Marketplace</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>Tokens</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300' >NexChat</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>About</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer' >Home</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer' >Features</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer' >Marketplace</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>Tokens</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer' >NexChat</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>About</li>
                                                 </ul>
                                                 <ul>
                                                         <li className=' pb-[10px] sm:pb-[20px]'><a className=' text-white font-Recharge font-bold text-[16px] sm:text-[18px] leading-[21px] ' href="">Information's</a></li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>Contact</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>Phone</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>terms</li>
-                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300'>Privacy</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>Contact</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>Phone</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>terms</li>
+                                                        <li className=' pb-[10px] sm:pb-[20px] font-Recharge font-bold text-[13px] sm:text-[16px] leading-[19px] text-[#B7B9B9] hover:text-white duration-300 cursor-pointer'>Privacy</li>
 
                                                 </ul>
                                         </div>
